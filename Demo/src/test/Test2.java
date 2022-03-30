bee17f7735bbb143456f5a0147e2d70e35369dd4
unknown <jivannagose97@gmail.com>
2022-03-30 11:56:32 +0530

    first commit by x

---
+++ Demo/src/test/Test2.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		//System.out.println("Welcome To Java ,This code for Git");
+		String test="Welcome to Java";
+		if(test.contains("Java"))
+		{
+			System.out.println("Found!!!!");
+		}
+	}
+
+}
